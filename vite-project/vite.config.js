d8305b330006725dd4a8c75d925834def155e9ef
CamilleJulieRais <rais.camille@gmail.com>
2026-03-13 11:15:48 +0100

    Feat(essai pour transformer en app utilisable sur Iphone/android)

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,10 +1,32 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-  },
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: "Ma ToDo List",
+        short_name: "ToDo",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#f4f3ff",
+        theme_color: "#7f56d9",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+    }),
+  ],
 });
